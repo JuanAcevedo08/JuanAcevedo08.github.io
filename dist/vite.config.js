6f6e18e7595940615342171d198247ae3346d9bd
Juan Acevedo <juandavidacevedon@gmail.com>
2025-07-16 11:17:23 -0500

    add

---
+++ dist/vite.config.js
@@ -0,0 +1,9 @@
+// filepath: /home/j/JuanAcevedo08.github.io/vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './docs',
+  build: {
+    outDir: './dist',
+  },
+});
